1fbcce92e960f4f0c52b179e36c1eba3c24595e6
MattKCyr <matt@mspacecreative.com>
2023-09-19 10:24:24 -0300

    CSS tweaks and sidebar template

--- functions/widgets.php
+++ functions/widgets.php
@@ -9,4 +9,14 @@ if (function_exists('register_sidebar')) {
         'before_title'  => '<h2>',
         'after_title'   => '</h2>'
     ));
+
+    register_sidebar(array(
+        'name' => __('Pages Sidebar', 'irc'),
+        'id'   => 'pages-sidebar',
+        'description'   => 'Widget Area for Page with Sidebar Template',
+        'before_widget' => '<div id="sidebar-%1$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2>',
+        'after_title'   => '</h2>'
+    ));
 }
---
+++ page-sidebar.php
@@ -0,0 +1,56 @@
+<?php
+/* Template Name: Sidebar Layout */
+get_header();
+
+$pagetitle = get_field('hide_page_title');
+$sidebar = get_field('show_sidebar');
+$boxedlayout = get_field('boxed_layout'); ?>
+
+<div class="content-wrapper">
+
+	<div class="inner_container bottom-padding">
+
+		<div class="row gutter_space_2">
+	
+			<main id="main-content" class="main-content col col-lg-9 col-md-9 col-sm-12 col-xs-12">
+				
+				<?php 
+				if (!is_page('home')) {
+				echo
+				'<div class="top-bottom-margin-2em">';
+				breadcrumbs();
+				echo
+				'</div>';
+				} 
+				if ( !$pagetitle ) : ?>
+				<h1><?php the_title(); ?></h1>
+				<?php endif; ?>
+			
+				<?php
+				if ( have_posts() ):
+				while ( have_posts() ): the_post();
+					
+				the_content();
+					
+				endwhile;
+					
+				else : ?>
+
+				<p>no posts found.</p>
+					
+				<?php endif; ?>
+
+			</main>
+
+			<aside class="top-bottom-margin-2em col col-lg-3 col-md-3 col-sm-12 col-xs-12">
+			<?php 
+			if ( ! dynamic_sidebar( 'pages-sidebar' ) ) {
+				dynamic_sidebar( 'pages-sidebar' );
+			} ?>
+			</aside>
+		</div>
+	</div>
+
+</div>
+
+<?php get_footer();

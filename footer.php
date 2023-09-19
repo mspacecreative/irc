6b8235383d5e8ca6979a4de67ddff6f722dadae1
MattKCyr <matt@mspacecreative.com>
2023-09-19 11:33:09 -0300

    c

--- footer.php
+++ footer.php
@@ -1,49 +1,50 @@
 		<footer>
-			<?php 
-			$footerwidth = get_field('footer_content_width', 'options');
-			switch ( $footerwidth ) {
-				case 'narrow':
-					$value = 'narrow';
-					break;
-				default:
-					$value = '';
-			} ?>
-			<?php 
-			if ( is_active_sidebar('footer-area-1') || is_active_sidebar('footer-area-2') || is_active_sidebar('footer-area-3') || is_active_sidebar('footer-area-4') ) {
-				echo
-				'<div class="row gutter_space_2">';
-				if ( ! dynamic_sidebar( 'footer-area-1' ) && is_active_sidebar( 'footer-area-1' ) ) {
+			<div class="inner_container">
+				<?php 
+				$footerwidth = get_field('footer_content_width', 'options');
+				switch ( $footerwidth ) {
+					case 'narrow':
+						$value = 'narrow';
+						break;
+					default:
+						$value = '';
+				} ?>
+				<?php 
+				if ( is_active_sidebar('footer-area-1') || is_active_sidebar('footer-area-2') || is_active_sidebar('footer-area-3') || is_active_sidebar('footer-area-4') ) {
 					echo
-					'<div class="col col-flex-auto">';
-					dynamic_sidebar( 'footer-area-1' );
+					'<div class="row gutter_space_2">';
+					if ( ! dynamic_sidebar( 'footer-area-1' ) && is_active_sidebar( 'footer-area-1' ) ) {
+						echo
+						'<div class="col col-flex-auto">';
+						dynamic_sidebar( 'footer-area-1' );
+						echo
+						'</div>';
+					}
+					if ( ! dynamic_sidebar( 'footer-area-2' ) && is_active_sidebar( 'footer-area-2' ) ) {
+						echo
+						'<div class="col col-flex-auto">';
+						dynamic_sidebar( 'footer-area-2' );
+						echo
+						'</div>';
+					}
+					if ( ! dynamic_sidebar( 'footer-area-3' ) && is_active_sidebar( 'footer-area-3' ) ) {
+						echo
+						'<div class="col col-flex-auto">';
+						dynamic_sidebar( 'footer-area-3' );
+						echo
+						'</div>';
+					}
+					if ( ! dynamic_sidebar( 'footer-area-4' ) && is_active_sidebar( 'footer-area-4' ) ) {
+						echo
+						'<div class="col col-flex-auto">';
+						dynamic_sidebar( 'footer-area-4' );
+						echo
+						'</div>';
+					}
 					echo
 					'</div>';
 				}
-				if ( ! dynamic_sidebar( 'footer-area-2' ) && is_active_sidebar( 'footer-area-2' ) ) {
-					echo
-					'<div class="col col-flex-auto">';
-					dynamic_sidebar( 'footer-area-2' );
-					echo
-					'</div>';
-				}
-				if ( ! dynamic_sidebar( 'footer-area-3' ) && is_active_sidebar( 'footer-area-3' ) ) {
-					echo
-					'<div class="col col-flex-auto">';
-					dynamic_sidebar( 'footer-area-3' );
-					echo
-					'</div>';
-				}
-				if ( ! dynamic_sidebar( 'footer-area-4' ) && is_active_sidebar( 'footer-area-4' ) ) {
-					echo
-					'<div class="col col-flex-auto">';
-					dynamic_sidebar( 'footer-area-4' );
-					echo
-					'</div>';
-				}
-				echo
-				'</div>';
-			}
-			?>
+				?>
 			</div>
 			<div class="light inner_container <?php echo $value ?>">
 				<p><?php echo __('&copy '); echo date('Y '); echo bloginfo('title'); echo __('. All rights reserved.'); ?></p>

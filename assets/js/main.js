863dad24497e5dda967b6f03c98f387d7b6d8971
MattKCyr <matt@mspacecreative.com>
2023-09-19 15:55:49 -0300

    Header tweaks

--- assets/js/main.js
+++ assets/js/main.js
@@ -206,3 +206,32 @@
     });
   });
 })(jQuery);
+
+// FIXED HEADER TOGGLE
+var lastScrollTop = 0;
+const header = document.querySelector("header");
+
+window.addEventListener(
+  "scroll",
+  function () {
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+      header.classList.add("scroll-down");
+      header.classList.remove("fixed", "scroll-up", "reset");
+    } else if (st < lastScrollTop) {
+      header.classList.add("fixed", "scroll-up");
+      header.classList.remove("scroll-down");
+      if (window.pageYOffset == 20) {
+        header.classList.add("reset");
+      }
+    } // else was horizontal scroll
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+  },
+  false
+);
+
+window.onscroll = function () {
+  if (window.pageYOffset <= 150) {
+    header.classList.add("reset");
+  }
+};

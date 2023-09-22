8fc74498680304339b4b87711f121508db2fa4f3
MattKCyr <matt@mspacecreative.com>
2023-09-22 09:47:55 -0300

    c

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 get_header();
 
 $pagetitle = get_field('hide_page_title');
-$date = the_date(__('F j, Y', 'irc')); ?>
+$date = wp_date(the_date(__('F j, Y', 'irc'))); ?>
 
 <div class="content-wrapper">
 

1b24abf5d578b351cc23deeeca293b1cb6a6e285
MattKCyr <matt@mspacecreative.com>
2023-09-25 14:04:15 -0300

    c

--- functions/breadcrumbs.php
+++ functions/breadcrumbs.php
@@ -20,3 +20,19 @@ add_filter( 'wpseo_breadcrumb_links', 'unbox_yoast_seo_breadcrumb_append_link' )
      }
      return $links;
  }
+
+ // ADDING SERVICES INDEX LINK TO BREADCRUMBS
+add_filter( 'wpseo_breadcrumb_links', 'unbox_yoast_seo_breadcrumb_append_link' );
+function unbox_yoast_seo_breadcrumb_append_link( $links ) {
+    global $post;
+    if( is_singular('services')){
+        $breadcrumb = array(
+           array(
+           'url' => site_url( '/services/' ),
+           'text' => 'Services',
+           )
+       );
+      array_splice($links, -1, 0, $breadcrumb); 
+    }
+    return $links;
+}

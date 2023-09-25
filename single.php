00ae96a9fb0148abed3a78f34d3024a50d536dc3
MattKCyr <matt@mspacecreative.com>
2023-09-25 15:51:26 -0300

    c

--- single.php
+++ single.php
@@ -70,7 +70,7 @@ $slug = $post_type == 'post' ? 'news' : $post_type; ?>
 						<?php 
 						if ($featuredimg) {
 							echo 
-							'<div class="hide-on-mobile bottom-margin-2em">'
+							'<div class="hide-on-mobile">'
 							 . get_the_post_thumbnail(get_the_ID(), 'large') . 
 							'</div>';
 						}

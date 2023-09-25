7f2ed239e4aa392111caf3f10a952cd41b85608d
MattKCyr <matt@mspacecreative.com>
2023-09-25 12:26:02 -0300

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,7 +204,7 @@ let subToggle = document.querySelectorAll(".sub-toggle");
 for (i = 0; i < subToggle.length; i++) {
   subToggle[i].addEventListener("click", function () {
     slideToggle(this.parentNode.children[2], 200);
-    subToggle[i].classList.toggle("is-active");
+    this.classList.toggle("is-active");
   });
 }
 

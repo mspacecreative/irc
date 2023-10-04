d1ff96b0798cf23b7c37f3ec5e47dc997a0a1c2f
MattKCyr <matt@mspacecreative.com>
2023-10-04 16:08:21 -0300

    c

--- templates/blocks/block-filter.php
+++ templates/blocks/block-filter.php
@@ -1,31 +1,13 @@
+<select>
 <?php
-// $args = array(
-//     'post_type' => 'business',
-//     // 'tax_query' => array(
-//     //     array(
-//     //         'taxonomy' => 'business-category'
-//     //     );
-//     // );
-// );
-
-// $loop = new WP_Query($args);
-
-// if ($loop->have_posts()) {
-    echo
-    '<select>';
-    // while ($loop->have_posts()) {
-    //     $loop->the_post();
-        $args = array(
-            'taxonomy'   => 'business-category',
-            'orderby'    => 'name',
-            'order'      => 'ASC',
-            'hide_empty' => false,
-        );
-        $loop = new WP_Term_Query($args);
-        foreach($loop->get_terms() as $term) {
-            echo '<option>' . $term->name . '</option>';
-        }
-    // }
-    echo
-    '</select>';
-// } wp_reset_query();
+$args = array(
+    'taxonomy'   => 'business-category',
+    'orderby'    => 'name',
+    'order'      => 'ASC',
+    'hide_empty' => false,
+);
+$loop = new WP_Term_Query($args);
+foreach($loop->get_terms() as $term) : ?>
+    <option><?php echo $term->name ?></option>
+<?php endif; ?>
+</select>

56682d3bb78a4f9a444d50c7537951b6c7b8434b
MattKCyr <matt@mspacecreative.com>
2023-10-04 20:42:53 -0300

    c

--- templates/blocks/block-loop.php
+++ templates/blocks/block-loop.php
@@ -42,8 +42,9 @@ $taxonomies = get_terms( array(
 
 if ( !empty($taxonomies) ) :
 	$output = '<select>';
-    $output.= '<optgroup label="'. esc_html__('Select') .'">';
+    $output.= '<optgroup label="'. esc_html__('Any') .'">';
     foreach( $taxonomies as $term ) {
+        $output.= '<option value="" disabled selected hidden>Any</option>';
         $output.= '<option value="'. esc_attr( $term->term_id ) .'">
             '. esc_html( $term->name ) .'</option>';
     }

614ae708b801bfc6c60d33684353a38fe54efa57
MattKCyr <matt@mspacecreative.com>
2023-10-04 20:55:47 -0300

    c

--- templates/blocks/block-loop.php
+++ templates/blocks/block-loop.php
@@ -42,7 +42,7 @@ $taxonomies = get_terms( array(
 
 if ( !empty($taxonomies) ) :
 	$output = '<select>';
-    $output.= '<option value="- Any -" selected>-Any-</option>';
+    $output.= '<option value="-&nbsp;Any&nbsp;-" selected>-Any-</option>';
     foreach( $taxonomies as $term ) {
         $output.= '<option value="'. esc_attr( $term->term_id ) .'">'. esc_html( $term->name ) .'</option>';
     }

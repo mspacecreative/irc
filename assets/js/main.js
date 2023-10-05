a919bd3bcda51bbec9a69bc16a5c222aa9a26f09
MattKCyr <matt@mspacecreative.com>
2023-10-05 16:06:34 -0300

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@
 
     function showOnChange() {
       const cards = document.querySelectorAll(".wp-block-post");
-      const filterSelect = document.getElementsByTagName("select").value;
+      const filterSelect = document.querySelector(".filter").value;
       for (var i = 0; i < cards.length; i++) {
         if (cards[i].classList.contains(filterSelect) || filterSelect == null) {
           cards[i].style.display = "block";

59d4e20c5aa982553b1d41e26057113d80908dc2
MattKCyr <matt@mspacecreative.com>
2023-10-05 11:44:51 -0300

    c

--- templates/blocks/block-loop.php
+++ templates/blocks/block-loop.php
@@ -90,7 +90,7 @@ $loop = new WP_Query( array(
     $email_address = get_field('email_address', get_the_ID());
     $mailing_address = get_field('mailing_address', get_the_ID());
 
-    $terms = strip_tags(strtolower(get_the_terms(get_the_ID(), array($taxonomy, $taxonomy2)));
+    $terms = get_the_terms(get_the_ID(), array($taxonomy, $taxonomy2));
     foreach ($terms as $term) {
         $terms_as_classes = $term->slug;
     }

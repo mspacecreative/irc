fff7b1ba8e4ec6ee7048f795d570f10996d5f74b
MattKCyr <matt@mspacecreative.com>
2023-10-10 11:33:07 -0300

    c

---
+++ archive-communities.php
@@ -0,0 +1,63 @@
+<?php
+get_header();
+
+$pagetitle = get_field('hide_page_title');
+$sidebar = get_field('show_sidebar');
+$boxedlayout = get_field('boxed_layout'); ?>
+
+<div class="content-wrapper">	
+	
+	<main id="main-content" class="main-content bottom-padding top-padding-2em">
+
+		<div class="inner_container">
+			<?php 
+			if (!is_page('home')) {
+			echo
+			'<div class="top-bottom-margin-2em">';
+			breadcrumbs();
+			echo
+			'</div>';
+			} 
+			if ( !$pagetitle ) : ?>
+			<h1><?php echo single_term_title('', false) ?></h1>
+			<?php endif; ?>
+		</div>
+	
+		<?php
+		if ( have_posts() ):
+		while ( have_posts() ): the_post();
+		
+		if ($boxedlayout) : ?>
+		<div class="inner_container">
+		<?php endif; ?>
+			
+		<?php 
+		$term_description = term_description();
+
+		echo $term_description; echo the_content();
+
+		if ($boxedlayout) : ?>
+		</div>
+		<?php endif; ?>
+			
+		<?php endwhile;
+			
+		else : ?>
+
+		<div class="inner_container">
+		<p>no posts found.</p>
+		</div>
+			
+		<?php endif; ?>
+
+	</main>
+
+	<?php if ($sidebar) : ?>
+	<aside>
+		 
+	</aside>
+	<?php endif; ?>
+
+</div>
+
+<?php get_footer();

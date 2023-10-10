d2524d67baff1a3f01e8fd4b7d2a735d237af472
MattKCyr <matt@mspacecreative.com>
2023-10-10 09:49:11 -0300

    c

--- templates/blocks/block-loop.php
+++ templates/blocks/block-loop.php
@@ -176,12 +176,15 @@ $loop = new WP_Query( array(
             '</div>';
         }
 
-        while (have_rows('staff_data', get_the_ID())) {
-            the_row();
-            $credentials = get_sub_field('credentials') ? __(', ') . get_sub_field('credentials') : '';
+        if (have_rows('staff_data', get_the_ID())) {
 
-            echo
-            '<h2 class="wp-block-post-title has-medium-font-size"' . $bottom_margin . '>' . esc_html__($title) . $credentials . '</h2>';
+            while (have_rows('staff_data', get_the_ID())) {
+                the_row();
+                $credentials = get_sub_field('credentials') ? __(', ') . get_sub_field('credentials') : '';
+
+                echo
+                '<h2 class="wp-block-post-title has-medium-font-size"' . $bottom_margin . '>' . esc_html__($title) . $credentials . '</h2>';
+            } 
         } else {
 
         echo

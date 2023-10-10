609a96a3984ddb3a8bf7f103cd567dad4fc27583
MattKCyr <matt@mspacecreative.com>
2023-10-10 09:42:16 -0300

    c

--- templates/blocks/block-loop.php
+++ templates/blocks/block-loop.php
@@ -179,10 +179,13 @@ $loop = new WP_Query( array(
         while (have_rows('staff-data')) {
             the_row();
             $credentials = get_sub_field('credentials') ? __(', ') . get_sub_field('credentials') : '';
+
+            echo
+            '<h2 class="wp-block-post-title has-medium-font-size"' . $bottom_margin . '>' . esc_html__($title) . $credentials . '</h2>';
         }
 
         echo
-        '<h2 class="wp-block-post-title has-medium-font-size"' . $bottom_margin . '>' . esc_html__($title) . $credentials . '</h2>';
+        '<h2 class="wp-block-post-title has-medium-font-size"' . $bottom_margin . '>' . esc_html__($title) . '</h2>';
 
         if ( have_rows('staff_data', get_the_ID()) ) {
         echo
